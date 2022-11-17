54b6fdf4a7f777ff98647be31bbcd5d639d454b8
Warren G <warrengdev@gmail.com>
2022-11-17 15:18:41 -0600

    Wireframe Added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
 
         <h1 className="title">The Food Test</h1>
 
-        <div className="test-container"></div>
+        <div className="test-container">
+          <div className="food-wrapper">
+            <img className="food-pic" src="../imgs/pizza.jpeg" alt="" />
+            <h2 className="food-title">Pizza</h2>
+          </div>
+          <div className="food-wrapper">
+            <img className="food-pic" src="../imgs/pizza.jpeg" alt="" />
+            <h2 className="food-title">Pizza</h2>
+          </div>
+        </div>
 
 
       </div>
