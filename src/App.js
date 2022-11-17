4c341f1c0446cc41d67962e4c581875e7cec40e3
Warren G <warrengdev@gmail.com>
2022-11-17 14:28:11 -0600

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="test-wrapper">
+
+        <h1 className="title">The Food Test</h1>
+
+        <div className="test-container"></div>
+
+
+      </div>
     </div>
   );
 }
